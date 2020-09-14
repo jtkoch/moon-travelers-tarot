d8028621286c1c7aba6a9039e69df4282e8071f0
jtkoch <jtkoch12@gmail.com>
2020-09-14 12:01:37 -0500

    added svg background

--- moon-travelers/src/components/Footer.js
+++ moon-travelers/src/components/Footer.js
@@ -3,39 +3,39 @@ import styled from 'styled-components'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faInstagram, faFacebookF } from "@fortawesome/free-brands-svg-icons"
 
+import footerlogo from '../images/Logo/logo9.png'
+
 const StickyFooter = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  align-items: center;
+  justify-content: space-around;
   height: 100px;
   width: 100%;
   padding: 1%;
 `
-
-const Site = styled.div`
-  font-size: 15px;
-  color: #a582b8;
-
-  @media (max-width: 500px) {
-    font-size: 12px;
-    padding: 5px;
-  }
+const SocialContainer = styled.div`
+  margin-bottom: 40px;
 `
 const Social = styled.a`
-  margin: 10px;
+  margin: 15px;
   color: #a582b8;
+`
+const Logo = styled.img`
+  width: 100px;
+  margin-bottom: 40px;
 `
 
 function Footer() {
   return (
     <StickyFooter className="mt-5">
-          <Site>
-            This site was made by Jensen Koch.
-          </Site>
           <div>
+            <Logo src={footerlogo} />
+          </div>
+          <SocialContainer>
             <Social href="https://www.facebook.com/Moon-Travelers-Tarot-220673835218164" target=" _blank" rel="noopener noreferrer" ><FontAwesomeIcon icon={faFacebookF}></FontAwesomeIcon></Social>
             <Social href="https://www.instagram.com/dunnette89/" target=" _blank" rel="noopener noreferrer" ><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></Social>
-          </div>
+          </SocialContainer>
     </StickyFooter>
   )
 }
