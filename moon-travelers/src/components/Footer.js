97cc95b13e928f0a9355fced2c0f45a33889db60
jtkoch <jtkoch12@gmail.com>
2020-09-15 15:55:06 -0500

    most styling/design done

--- moon-travelers/src/components/Footer.js
+++ moon-travelers/src/components/Footer.js
@@ -15,15 +15,23 @@ const StickyFooter = styled.div`
   padding: 1%;
 `
 const SocialContainer = styled.div`
-  margin-bottom: 40px;
+  margin-bottom: .5rem;
 `
 const Social = styled.a`
   margin: 15px;
   color: #a582b8;
 `
+const Site = styled.p`
+  font-size: .75rem;
+  margin-bottom: .5rem;
+
+  @media (max-width: 500px) {
+    font-size: .5rem;
+  }
+`
 const Logo = styled.img`
   width: 100px;
-  margin-bottom: 40px;
+  margin-bottom: .5rem;
 `
 
 function Footer() {
@@ -32,10 +40,13 @@ function Footer() {
           <div>
             <Logo src={footerlogo} />
           </div>
+          <Site>Website by Jensen Koch</Site>
           <SocialContainer>
             <Social href="https://www.facebook.com/Moon-Travelers-Tarot-220673835218164" target=" _blank" rel="noopener noreferrer" ><FontAwesomeIcon icon={faFacebookF}></FontAwesomeIcon></Social>
             <Social href="https://www.instagram.com/dunnette89/" target=" _blank" rel="noopener noreferrer" ><FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon></Social>
           </SocialContainer>
+
+          
     </StickyFooter>
   )
 }
